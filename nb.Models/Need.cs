40a31cd24dc13b5971dea506232fddbfc0925c86
Joel Musheno <joelmusheno@gmail.com>
2016-08-28 08:30:58 -0400

    sketching in test

--- nb.Models/Need.cs
+++ nb.Models/Need.cs
@@ -3,12 +3,11 @@ using nb.Models.Foundation;
 
 namespace nb.Models
 {
-	public class Need:IBaseModel
+	public class Need : IBaseModel
 	{
-		public int Id { get; set;}
+		public int Id { get; set; }
 		public DateTime CreateDate { get; set; }
 		public string Name { get; set; }
 		public Location Location { get; set; }
 	}
 }
-
---
+++ nb.Tests/NeedRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using nb;
+using nb.Controllers;
+using nb.Infrastructure;
+using nb.Models;
+
+namespace nb.Tests
+{
+	[TestFixture]
+	public class NeedRepositoryTest
+	{
+		[Test]
+		public void CanVarUpNewRepo()
+		{
+			var repo = new InMemoryRepository<Need>();
+			Assert.IsNotNull(repo);
+		}
+	}
+}
